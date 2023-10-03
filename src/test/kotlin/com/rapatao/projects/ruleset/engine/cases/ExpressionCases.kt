c7d80d9d309066c0480f30fdbf13579c19e4423d
Luiz Henrique Rapatao <rapatao@rapatao.com>
2023-10-03 12:32:49 +0100

    create BetweenBuilder and refactor ExpressionCases

--- src/test/kotlin/com/rapatao/projects/ruleset/engine/cases/ExpressionCases.kt
+++ src/test/kotlin/com/rapatao/projects/ruleset/engine/cases/ExpressionCases.kt
@@ -3,6 +3,8 @@ package com.rapatao.projects.ruleset.engine.cases
 import com.rapatao.projects.ruleset.engine.types.OnFailure
 import com.rapatao.projects.ruleset.engine.types.builder.asExpression
 import com.rapatao.projects.ruleset.engine.types.builder.equalsTo
+import com.rapatao.projects.ruleset.engine.types.builder.from
+import com.rapatao.projects.ruleset.engine.types.builder.fromInclusive
 import com.rapatao.projects.ruleset.engine.types.builder.greaterOrEqualThan
 import com.rapatao.projects.ruleset.engine.types.builder.greaterThan
 import com.rapatao.projects.ruleset.engine.types.builder.ifFail
@@ -15,7 +17,121 @@ import org.junit.jupiter.params.provider.Arguments
 object ExpressionCases {
 
     @Suppress("LongMethod")
-    fun cases(): List<Arguments> = listOf(
+    fun cases(): List<Arguments> = asExpressionCases() +
+        equalsCases() +
+        booleanCases() +
+        ifFailCases() +
+        betweenCases() +
+        greaterThanCases() +
+        lessThanCases() +
+        lessOrEqualThanCases() +
+        greaterOrEqualThanCases()
+
+    private fun greaterOrEqualThanCases() = listOf(
+        Arguments.of(
+            "item.price" greaterOrEqualThan 1,
+            true
+        ),
+        Arguments.of(
+            "item.price" greaterOrEqualThan 10,
+            true
+        ),
+        Arguments.of(
+            "item.price" greaterOrEqualThan 100,
+            false
+        ),
+        Arguments.of(
+            10 greaterOrEqualThan 1,
+            true
+        ),
+        Arguments.of(
+            10 greaterOrEqualThan 10,
+            true
+        ),
+        Arguments.of(
+            10 greaterOrEqualThan 100,
+            false
+        ),
+    )
+
+    private fun lessOrEqualThanCases() = listOf(
+        Arguments.of(
+            "item.price" lessOrEqualThan 100,
+            true
+        ),
+        Arguments.of(
+            "item.price" lessOrEqualThan 10,
+            true
+        ),
+        Arguments.of(
+            "item.price" lessOrEqualThan 1,
+            false
+        ),
+        Arguments.of(
+            10 lessOrEqualThan 100,
+            true
+        ),
+        Arguments.of(
+            10 lessOrEqualThan 10,
+            true
+        ),
+        Arguments.of(
+            10 lessOrEqualThan 1,
+            false
+        ),
+    )
+
+    private fun lessThanCases() = listOf(
+        Arguments.of(
+            "item.price" lessThan 100,
+            true
+        ),
+        Arguments.of(
+            "item.price" lessThan 1,
+            false
+        ),
+        Arguments.of(
+            "item.price" lessThan 10,
+            false
+        ),
+        Arguments.of(
+            2 lessThan 10,
+            true
+        ),
+        Arguments.of(
+            20 lessThan 10,
+            false
+        ),
+        Arguments.of(
+            10 lessThan 10,
+            false
+        ),
+    )
+
+    private fun greaterThanCases() = listOf(
+        Arguments.of(
+            "item.price" greaterThan 0,
+            true
+        ),
+        Arguments.of(
+            "item.price" greaterThan 10,
+            false
+        ),
+        Arguments.of(
+            "item.price" greaterThan 20,
+            false
+        ),
+        Arguments.of(
+            10 greaterThan 20,
+            false
+        ),
+        Arguments.of(
+            30 greaterThan 20,
+            true
+        ),
+    )
+
+    private fun asExpressionCases() = listOf(
         Arguments.of(
             "item.price <= 1000".asExpression(),
             true
@@ -24,12 +140,34 @@ object ExpressionCases {
             "item.price >= 1000".asExpression(),
             false
         ),
+    )
+
+    private fun equalsCases() = listOf(
         Arguments.of(
             "item.price" equalsTo 10,
             true
         ),
         Arguments.of(
             "item.price" equalsTo 0,
+            false
+        ),
+    )
+
+    private fun booleanCases() = listOf(
+        Arguments.of(
+            true.isFalse(),
+            false
+        ),
+        Arguments.of(
+            false.isFalse(),
+            true
+        ),
+        Arguments.of(
+            true.isTrue(),
+            true
+        ),
+        Arguments.of(
+            false.isTrue(),
             false
         ),
         Arguments.of(
@@ -48,118 +186,9 @@ object ExpressionCases {
             "item.falseValue".isFalse(),
             true
         ),
-        Arguments.of(
-            "item.price" greaterThan 0,
-            true
-        ),
-        Arguments.of(
-            "item.price" greaterThan 10,
-            false
-        ),
-        Arguments.of(
-            "item.price" greaterOrEqualThan 10,
-            true
-        ),
-        Arguments.of(
-            "item.price" greaterOrEqualThan 100,
-            false
-        ),
-        Arguments.of(
-            "item.price" lessThan 100,
-            true
-        ),
-        Arguments.of(
-            "item.price" lessThan 1,
-            false
-        ),
-        Arguments.of(
-            "item.price" lessOrEqualThan 100,
-            true
-        ),
-        Arguments.of(
-            "item.price" lessOrEqualThan 10,
-            true
-        ),
-        Arguments.of(
-            "item.price" lessOrEqualThan 1,
-            false
-        ),
-        Arguments.of(
-            "item.price" equalsTo 1,
-            false
-        ),
-        Arguments.of(
-            "item.price" equalsTo 10,
-            true
-        ),
-        Arguments.of(
-            "item.falseValue".isTrue(),
-            false,
-        ),
-        Arguments.of(
-            "item.falseValue".isFalse(),
-            true
-        ),
-        Arguments.of(
-            true.isFalse(),
-            false
-        ),
-        Arguments.of(
-            false.isFalse(),
-            true
-        ),
-        Arguments.of(
-            true.isTrue(),
-            true
-        ),
-        Arguments.of(
-            false.isTrue(),
-            false
-        ),
-        Arguments.of(
-            "item.price" greaterThan 0,
-            true
-        ),
-        Arguments.of(
-            "item.price" greaterThan 10,
-            false
-        ),
-        Arguments.of(
-            "item.price" greaterOrEqualThan 10,
-            true
-        ),
-        Arguments.of(
-            "item.price" greaterOrEqualThan 100,
-            false
-        ),
-        Arguments.of(
-            "item.price" lessOrEqualThan 100,
-            true
-        ),
-        Arguments.of(
-            "item.price" lessOrEqualThan 10,
-            true
-        ),
-        Arguments.of(
-            "item.price" lessOrEqualThan 1,
-            false
-        ),
-        Arguments.of(
-            "item.price" lessThan 100,
-            true
-        ),
-        Arguments.of(
-            "item.price" lessThan 1,
-            false
-        ),
-        Arguments.of(
-            "item.price <= 1000".asExpression(),
-            true
-        ),
-        Arguments.of(
-            "item.price >= 1000".asExpression(),
-            false
-        ),
+    )
+
+    private fun ifFailCases() = listOf(
         Arguments.of(
             "item.field.that.dont.exist" equalsTo "10" ifFail OnFailure.TRUE,
             true
@@ -167,6 +196,89 @@ object ExpressionCases {
         Arguments.of(
             "item.field.that.dont.exist" equalsTo "10" ifFail OnFailure.FALSE,
             false
+        ),
+    )
+
+    private fun betweenCases() = listOf(
+        Arguments.of(
+            "item.price" from 5 to 15,
+            true
+        ),
+        Arguments.of(
+            "item.price" from 5 toInclusive 15,
+            true
+        ),
+        Arguments.of(
+            "item.price" fromInclusive 5 to 15,
+            true
+        ),
+        Arguments.of(
+            "item.price" fromInclusive 5 toInclusive 15,
+            true
+        ),
+        Arguments.of(
+            "item.price" from 5 to 9,
+            false
+        ),
+        Arguments.of(
+            "item.price" from 5 toInclusive 9,
+            false
+        ),
+        Arguments.of(
+            "item.price" fromInclusive 5 to 9,
+            false
+        ),
+        Arguments.of(
+            "item.price" fromInclusive 5 toInclusive 9,
+            false
+        ),
+        Arguments.of(
+            "item.price" from 11 to 15,
+            false
+        ),
+        Arguments.of(
+            "item.price" from 11 toInclusive 15,
+            false
+        ),
+        Arguments.of(
+            "item.price" fromInclusive 11 to 15,
+            false
+        ),
+        Arguments.of(
+            "item.price" fromInclusive 11 toInclusive 15,
+            false
+        ),
+        Arguments.of(
+            "item.price" from 10 to 15,
+            false
+        ),
+        Arguments.of(
+            "item.price" from 10 toInclusive 15,
+            false
+        ),
+        Arguments.of(
+            "item.price" fromInclusive 10 to 15,
+            true
+        ),
+        Arguments.of(
+            "item.price" fromInclusive 10 toInclusive 15,
+            true
+        ),
+        Arguments.of(
+            "item.price" from 5 to 10,
+            false
+        ),
+        Arguments.of(
+            "item.price" from 5 toInclusive 10,
+            true
+        ),
+        Arguments.of(
+            "item.price" fromInclusive 5 to 10,
+            false
+        ),
+        Arguments.of(
+            "item.price" fromInclusive 5 toInclusive 10,
+            true
         ),
     )
 }
