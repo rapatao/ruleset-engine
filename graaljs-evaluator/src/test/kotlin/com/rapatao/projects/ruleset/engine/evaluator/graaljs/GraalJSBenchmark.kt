80cae9533640e1c0804e9d65eb29db7de0bff0a9
Luiz Henrique Rapatao <rapatao@rapatao.com>
2024-06-09 16:03:07 +0100

    feat: write benchmark output to a file

---
+++ graaljs-evaluator/src/test/kotlin/com/rapatao/projects/ruleset/engine/evaluator/graaljs/GraalJSBenchmark.kt
@@ -0,0 +1,7 @@
+package com.rapatao.projects.ruleset.engine.evaluator.graaljs
+
+import com.rapatao.projects.ruleset.engine.BaseEngineBenchmark
+
+fun main(args: Array<String>) {
+    BaseEngineBenchmark(GraalJSEvalEngine()).main(args)
+}
--- tests/src/main/kotlin/com/rapatao/projects/ruleset/engine/BaseEngineBenchmark.kt
+++ tests/src/main/kotlin/com/rapatao/projects/ruleset/engine/BaseEngineBenchmark.kt
@@ -3,13 +3,23 @@ package com.rapatao.projects.ruleset.engine
 import com.rapatao.projects.ruleset.engine.cases.TestData
 import com.rapatao.projects.ruleset.engine.context.EvalEngine
 import com.rapatao.projects.ruleset.engine.types.Expression
+import java.nio.file.Paths
+import kotlin.io.path.appendText
+import kotlin.io.path.createFile
+import kotlin.io.path.exists
+import kotlin.io.path.writeText
 import kotlin.time.Duration
 import kotlin.time.DurationUnit
 import kotlin.time.measureTimedValue
 
 class BaseEngineBenchmark(private val evalEngine: EvalEngine) {
+
+    private val benchOut = Paths.get("bench_${evalEngine.name()}.txt")
+
     @Suppress("MagicNumber")
     fun main(args: Array<String>) {
+
+        cleanup()
 
         val cases = TestData.cases()
             .map { it.get().first { arg -> arg is Expression } }
@@ -18,16 +28,16 @@ class BaseEngineBenchmark(private val evalEngine: EvalEngine) {
         val evaluator = Evaluator(engine = evalEngine)
 
         // ini: warmup
-        println("warmup ${evaluator.engine().name()}: start")
+        appendLine("warmup ${evaluator.engine().name()}: start")
         repeat(100) { cases.forEach { expression -> evaluator.evaluate(expression, TestData.inputData) } }
-        println("warmup ${evaluator.engine().name()}: done")
+        appendLine("warmup ${evaluator.engine().name()}: done")
         // end: warmup
 
         val times = mutableListOf<Duration>()
 
         val iterations = args.firstOrNull()?.let { Integer.parseInt(it) } ?: 1000
 
-        println()
+        appendLine()
 
         repeat(iterations) {
             val time =
@@ -39,28 +49,44 @@ class BaseEngineBenchmark(private val evalEngine: EvalEngine) {
 
             times.add(time.duration)
         }
-        println()
+        appendLine()
 
-        println()
+        appendLine()
 
         val total = times.reduce { acc, duration -> acc + duration }
 
-        println("$evalEngine> iterations: $iterations")
-        println("    ops: " + (iterations * cases.size))
-        println("  ops/s: " + ((iterations * cases.size) / total.toDouble(DurationUnit.SECONDS)))
-        println("  total: $total")
-        println("    max: " + times.max())
-        println("    min: " + times.min())
-        println("    avg: " + (total / times.size))
+        appendLine("$evalEngine> iterations: $iterations")
+        appendLine("    ops: " + (iterations * cases.size))
+        appendLine("  ops/s: " + ((iterations * cases.size) / total.toDouble(DurationUnit.SECONDS)))
+        appendLine("  total: $total")
+        appendLine("    max: " + times.max())
+        appendLine("    min: " + times.min())
+        appendLine("    avg: " + (total / times.size))
 
         val sortedResults = times.sorted()
         listOf(0.50, 0.75, 0.90, 0.95, 0.99).forEach { p ->
-            println(
+            appendLine(
                 "    p${(p * 100).toInt()}: " + sortedResults[(sortedResults.size * p).toInt()
                     .coerceAtMost(sortedResults.lastIndex)]
             )
         }
 
-        println()
+        appendLine()
+    }
+
+    private fun append(value: String) {
+        benchOut.appendText(value)
+        print(value)
+    }
+
+    private fun appendLine(value: String? = "") {
+        append(value + "\n")
+    }
+
+    private fun cleanup() {
+        if (!benchOut.exists()) {
+            benchOut.createFile()
+        }
+        benchOut.writeText("")
     }
 }
