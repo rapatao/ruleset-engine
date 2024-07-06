5d09a31ac3f0a5f476247c235268f4bbd0b3598f
Luiz Henrique Rapatao <rapatao@rapatao.com>
2024-07-06 10:55:08 +0100

    feat: remove Operator enum reference from kotlin module

--- kotlin-evaluator/src/main/kotlin/com/rapatao/projects/ruleset/engine/evaluator/kotlin/Parser.kt
+++ kotlin-evaluator/src/main/kotlin/com/rapatao/projects/ruleset/engine/evaluator/kotlin/Parser.kt
@@ -1,21 +1,20 @@
 package com.rapatao.projects.ruleset.engine.evaluator.kotlin
 
 import com.rapatao.projects.ruleset.engine.types.Expression
-import com.rapatao.projects.ruleset.engine.types.Operator
 
 internal object Parser {
     fun parse(expression: Expression, left: Any?, right: Any?) =
         when (expression.operator) {
-            Operator.EQUALS -> left == right
-            Operator.NOT_EQUALS -> left != right
-            Operator.GREATER_THAN -> left.comparable() > right
-            Operator.GREATER_OR_EQUAL_THAN -> left.comparable() >= right
-            Operator.LESS_THAN -> left.comparable() < right
-            Operator.LESS_OR_EQUAL_THAN -> left.comparable() <= right
-            Operator.STARTS_WITH -> left.toString().startsWith(right.toString())
-            Operator.ENDS_WITH -> left.toString().endsWith(right.toString())
-            Operator.CONTAINS -> left.checkContains(right)
-            null -> error("when evaluation an expression, the operator cannot be null")
+            "equals" -> left == right
+            "not_equals" -> left != right
+            "greater_than" -> left.comparable() > right
+            "greater_or_equal_than" -> left.comparable() >= right
+            "less_than" -> left.comparable() < right
+            "less_or_equal_than" -> left.comparable() <= right
+            "starts_with" -> left.toString().startsWith(right.toString())
+            "ends_with" -> left.toString().endsWith(right.toString())
+            "contains" -> left.checkContains(right)
+            else -> error("when evaluation an expression, the operator cannot be null")
         }
 
     @Suppress("UNCHECKED_CAST")
