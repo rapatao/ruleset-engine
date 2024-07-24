c41efa4ac0defa6189f2da31eeaee814c4497f4e
Luiz H. Rapatão <rapatao@rapatao.com>
2024-07-24 23:37:25 +0100

    Update rhino-evaluator/src/main/kotlin/com/rapatao/projects/ruleset/engine/evaluator/rhino/operator/NotContains.kt

--- rhino-evaluator/src/main/kotlin/com/rapatao/projects/ruleset/engine/evaluator/rhino/operator/NotContains.kt
+++ rhino-evaluator/src/main/kotlin/com/rapatao/projects/ruleset/engine/evaluator/rhino/operator/NotContains.kt
@@ -3,7 +3,7 @@ package com.rapatao.projects.ruleset.engine.evaluator.rhino.operator
 import com.rapatao.projects.ruleset.engine.context.EvalContext
 import com.rapatao.projects.ruleset.engine.types.operators.NotContainsOperator
 
-class NotContains : NotContainsOperator() {
+internal class NotContains : NotContainsOperator() {
     private val containsOperator = Contains()
 
     override fun process(context: EvalContext, left: Any?, right: Any?): Boolean =
